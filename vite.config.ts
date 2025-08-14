e33bbfc29993c79130066377b5465eb7e2b19f45
josue <gutabarwanimanajosue46@gmail.com>
2025-08-14 13:35:30 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,10 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite'
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
-});
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: './', // ✅ This makes sure assets load correctly in production
+  build: {
+    outDir: 'dist', // ✅ Default Vite build output folder
+  },
+})
+
